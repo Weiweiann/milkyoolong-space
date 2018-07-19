4f0b5d701e788a245d72560a1e09c5d9127340e6
Weiann <stargazer.wang@gmail.com>
2018-07-20 00:51:40 +0800

    router for editor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticleComponent } from './article/article.component';
 import { LoginComponent } from './login/login.component';
 import { EditorComponent } from './editor/editor.component';
 
 const routes: Routes = [
+  { path: '', component: ArticleComponent },
   { path: 'login', component: LoginComponent },
   { path: 'editor', component: EditorComponent },
 ];
